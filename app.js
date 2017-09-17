6732d5c5c6b77bb892c5de3ba28fad19541fcb72
Sayantankhan <iamstk14@gmail.com>
2017-09-17 22:04:10 +0530

    Add files via upload
    new feature added... use od directives

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ app.config(function($routeProvider,$locationProvider) {
 
     $routeProvider
     .when("/",{
-      templateUrl: "home1.html"
+      templateUrl: "home1.html",
+      controller: "PrintMessage"
     })
     .when("/home", {
-        templateUrl: "home1.html"
+        templateUrl: "home1.html",
+        controller: "TimeClock"
     })
     .when("/about_page", {
         templateUrl : "about.html",
         controller: "HelloWorldController"
     })
     .when("/red", {
-        templateUrl : "home1.html"
+        templateUrl : "home1.html",
+        controller: "PrintMessage"
     });
 
   $locationProvider.html5Mode(true).hashPrefix('!');
