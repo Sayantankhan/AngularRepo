aa012be71fe9dea7b3b5be7de83287aa0428e6a5
Sayantankhan <iamstk14@gmail.com>
2017-09-17 22:04:51 +0530

    Add files via upload
    updation of controller

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,22 @@
+"use strict";
+
 var app = angular.module('control', []);
+
 app.controller('HelloWorldController',function($scope){
   $scope.message = "HelloWorld!!!";
+});
+
+app.controller('PrintMessage',function($scope, $location){
+    $scope.myUrl = $location.absUrl();
+    $scope.message = "- Signup";
+    $scope.animal = "Animal";
+});
+
+app.controller('TimeClock',function($scope,$interval){
+
+  $scope.clock = new Date().toLocaleTimeString();
+  $interval(function () {
+      $scope.clock = new Date().toLocaleTimeString();
+  }, 1000);
+
 });
